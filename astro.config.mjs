a6952de4f3df90090c9c4ad8a4c73f0fd8854951
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2025-08-18 09:54:26 +0200

    x

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,8 +10,8 @@ const forceGitHubPages = process.env.DEPLOY_TARGET === 'github';
 const forceCustomDomain = process.env.DEPLOY_TARGET === 'custom';
 const useGitHubConfig = forceGitHubPages || (process.env.CI && !process.env.CUSTOM_DOMAIN && !forceCustomDomain);
 
-// Dominio principale: tale.mobi
-const siteUrl = forceCustomDomain ? 'https://tale.mobi' : (useGitHubConfig ? 'https://keyserdsoze.github.io' : 'https://tale.mobi');
+// Dominio principale: tale.ws (altri domini: tales.ws, tale.mobi, tales.mobi)
+const siteUrl = forceCustomDomain ? 'https://tale.ws' : (useGitHubConfig ? 'https://keyserdsoze.github.io' : 'https://tale.ws');
 const baseUrl = (useGitHubConfig && !forceCustomDomain) ? '/tale' : '';
 
 // https://astro.build/config
